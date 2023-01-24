3d58cd5bfaacf8634087543d3ec201bd0db9e47d
sebbanwalker <seb10walker@hotmail.com>
2023-01-24 23:22:35 +0100

    Tuesday

--- hangman-assignment/js/index.js
+++ hangman-assignment/js/index.js
@@ -15,7 +15,12 @@ const words = ['banana', 'sandwich', 'cheeseburger', 'helicopter', 'tsunami', 'r
 'hangman', 'farmer', 'airplane', 'school'];
 let unused = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-let used = [];
+let used;
+let ul;
+let wrongGuessesLeft = 5;
+let emptyLeft;
+
+//Eventlyssnare för skrivbordsinput, endast engelska alfabetet stöds
 
  document.addEventListener('keydown', function(event) { 
 
@@ -25,9 +30,10 @@ let used = [];
 
     if(keyPressed == unused[unused.indexOf(keyPressed)]) {
         console.log(unused.splice(unused.indexOf(keyPressed),1));
+        wordTracker();
     }
     else {
-        console.log("This letter has already been used.");
+        console.log("This letter has already been used/not allowed.");
     }
 });
 
@@ -38,9 +44,8 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
 function wordBuilder() {
-    let ul = document.getElementById('word');
+    ul = document.getElementById('word');
     for(let i = 0; i < chosenWord.length; i++) {
-        let letter = i;
         let li = document.createElement('li');
         li.appendChild(document.createTextNode(' '));
         ul.appendChild(li);
@@ -48,19 +53,79 @@ function wordBuilder() {
     }
 }
 
-function letterCheck() {
+// Metoden som kollar gentemot sökta ordets bokstäver och byter ut HTML. 
+
+function wordTracker() {
+    let list = ul.getElementsByTagName('li');
+    let correct = false;
     for(let i = 0; i < chosenWord.length; i++) {
-        for (let j = 0; j < unused.length; j++) {
-            if (chosenWord.charAt(i) == unused[j]) {
-                let li = document.createElement('li');
-                li.appendChild(document.createTextNode(unused[j]));
-                ul.appendChild(li);
-                console.log('swapped ' + unused[j]);
-            }
+        
+        if (chosenWord.charAt(i) == keyPressed) {
+            list[i].innerHTML = keyPressed;
+            console.log("Have swapped letter " + keyPressed);
+            correct = true;
+
         }
+        
+    }
+
+    for(let i = 0; i < chosenWord.length; i++) {
+        emptyLeft = 0;
+        if (list[i].innerHTML == ' ') {
+            emptyLeft++;
+            console.log("spaces found");
+
+        } 
+        else {
+            emptyLeft = false;
+            console.log("no spaces found");
+        }
+    }
+
+
+
+    if (correct == false) {
+        wrongGuessesLeft--;
+        lives(wrongGuessesLeft);
+        used = document.getElementById('nomatch');
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(keyPressed));
+        used.appendChild(li);
+    }
+    else  {
+        correct == false;
+    }
+
+    if(emptyLeft == 0) {
+        document.getElementById("game-won").style.display = "flex";
     }
 }
 
-function wordTracker() {
+//Switch som håller koll på liven
+
+function lives(wrongGuessesLeft) {
+    switch (wrongGuessesLeft) {
+        case 4: 
+            console.log("4 lives left.");
+            document.querySelector('figure').classList.add('scaffold');
+            break;
+        case 3: 
+            console.log("3 lives left.");
+            document.querySelector('figure').classList.add('head');
+            break;
+        case 2: 
+            console.log("2 lives left.");
+            document.querySelector('figure').classList.add('body');
+            break;
+        case 1: 
+            console.log("1 lives left.");
+            document.querySelector('figure').classList.add('arms');
+            break;
+        case 0: 
+            console.log("0 lives left.");
+            document.querySelector('figure').classList.add('legs');
+            document.getElementById("game-over").style.display = "flex";
+            break;
+    }
 }
 
